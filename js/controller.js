a026a0238a243fa30401a272a2add038467e77bd
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-08-28 23:40:53 -0300

    Desenvolvimento do arquivo 'controller.js'

--- js/controller.js
+++ js/controller.js
@@ -1,2 +1,10 @@
 var melhoresAtletasApp = angular.module('melhoresAtletasApp', []);
 
+melhoresAtletasApp.controller('AtletaController', function AtletaController($scope) {
+    
+    $scope.atleta = {
+        'nome' : 'Michael Phelps',
+        'categoria' : 'Nadador',
+        'pais' : 'Estados Unidos'
+    }
+});
